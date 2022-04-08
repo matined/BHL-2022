1a53cc9866afa813fc4c0ace73945f718abfea9b
Antoni Zajko <antoni.zajko.1@gmail.com>
2022-04-08 20:58:14 +0200

    Add categories processing

---
+++ pipelines/category_encoder.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+from sklearn.base import TransformerMixin
+from sklearn.preprocessing import OneHotEncoder
+
+
+class CategoryEncoder(TransformerMixin):
+
+    def fit(self, X, y=None, **kwargs):
+        self.one_hot_cols = ['BLDGCL', 'EXT', 'EASEMENT', 'EXCD2']
+        self.one_hot_transformer = OneHotEncoder(sparse=False)
+        self.one_hot_transformer.fit(X[self.one_hot_cols].astype('str'))
+        return self
+
+    def map_exemption(self, ex):
+        if ex == 'No':
+            return 0
+        else:
+            return ex[1]
+
+    def transform(self, X, y=None, **kwargs):
+        X = X.copy()
+        X['EXMPTCL'] = X['EXMPTCL'].apply(self.map_exemption)
+        one_hot_transformed = self.one_hot_transformer.transform(
+            X[self.one_hot_cols].astype('str')
+        )
+        one_hot_df = pd.DataFrame(one_hot_transformed,
+                                  columns=self.one_hot_transformer.get_feature_names(
+                                      self.one_hot_transformer.feature_names_in_
+                                  )
+                                  )
+        return pd.concat([X, one_hot_df], axis='columns')
